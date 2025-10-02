1771098e6937165a2389a693415c859cfc336deb
bharanidharan_r <bharanivijay69@gmail.com>
2025-10-03 01:34:56 +0530

    Added basic chat client and chat controller

---
+++ src/main/java/dev/bharani/spring_ai_support_bot/config/ChatClientConfig.java
@@ -0,0 +1,16 @@
+package dev.bharani.spring_ai_support_bot.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder
+                .build();
+    }
+
+}
---
+++ src/main/java/dev/bharani/spring_ai_support_bot/controller/ChatController.java
@@ -0,0 +1,25 @@
+package dev.bharani.spring_ai_support_bot.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String userPrompt) {
+        return chatClient.prompt()
+                .user(userPrompt)
+                .call()
+                .content();
+    }
+
+}
