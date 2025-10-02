fcbbc5e7d2a2388875b7811ed36609801bd74b29
bharanidharan_r <bharanivijay69@gmail.com>
2025-10-03 01:16:47 +0530

    Base project

---
+++ src/main/java/dev/bharani/spring_ai_support_bot/SpringAiSupportBotApplication.java
@@ -0,0 +1,13 @@
+package dev.bharani.spring_ai_support_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiSupportBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiSupportBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bharani/spring_ai_support_bot/SpringAiSupportBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bharani.spring_ai_support_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiSupportBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
