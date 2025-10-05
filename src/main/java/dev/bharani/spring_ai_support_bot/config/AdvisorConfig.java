35dc31e0308583f77b968dae33d51eef9f6be546
bharanidharan_r <bharanivijay69@gmail.com>
2025-10-06 00:13:24 +0530

    Added Rag support

--- src/main/java/dev/bharani/spring_ai_support_bot/config/AdvisorConfig.java
+++ src/main/java/dev/bharani/spring_ai_support_bot/config/AdvisorConfig.java
@@ -1,9 +1,11 @@
 package dev.bharani.spring_ai_support_bot.config;
 
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,12 @@ public class AdvisorConfig {
     @Bean
     public PromptChatMemoryAdvisor promptChatMemoryAdvisor() {
         return PromptChatMemoryAdvisor.builder(getChatMemory()).build();
+    }
+
+    @Bean
+    public QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore /*auto-configured*/) {
+        return QuestionAnswerAdvisor.builder(vectorStore)
+                .build();
     }
 
     private ChatMemory getChatMemory() {
--- src/main/java/dev/bharani/spring_ai_support_bot/config/ChatClientConfig.java
+++ src/main/java/dev/bharani/spring_ai_support_bot/config/ChatClientConfig.java
@@ -3,6 +3,9 @@ package dev.bharani.spring_ai_support_bot.config;
 import dev.bharani.spring_ai_support_bot.chat.Prompt;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +14,11 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder,
-                                 PromptChatMemoryAdvisor promptChatMemoryAdvisor) {
+                                 PromptChatMemoryAdvisor promptChatMemoryAdvisor,
+                                 QuestionAnswerAdvisor questionAnswerAdvisor) {
         return builder
                 .defaultSystem(Prompt.SYSTEM_PROMPT)
-                .defaultAdvisors(promptChatMemoryAdvisor)
+                .defaultAdvisors(promptChatMemoryAdvisor, questionAnswerAdvisor)
                 .build();
     }
 
